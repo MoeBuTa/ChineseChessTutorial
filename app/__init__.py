5a8ef3c9df4af8e1db4d078b4741eab3e353e8d5
Wenxiao <64770502+MoeBuTa@users.noreply.github.com>
2021-04-23 23:59:57 +0800

    improved project structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,42 +1,59 @@
-import os
+# import os
+#
+# import posts as posts
+#
+# from . import db
+# from flask import Flask, render_template
 
-import posts as posts
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-from . import db
-from flask import Flask, render_template
-
-
-
-
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route("/")
-    @app.route("/home")
-    def home():
-        return render_template('index.html', posts=posts, title='XiangQi / Chinese Chess')
+# __name__ : predefined variable in python, which is set to the name of the module in which it is used
+#  flask can use it as a starting point of loading associated resources such as template files
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
-    db.init_app(app)
-    from . import auth
-    app.register_blueprint(auth.bp)
-    return app
+from app import routes, models
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+# def create_app(test_config=None):
+#     # create and configure the app
+#     app = Flask(__name__, instance_relative_config=True)
+#     app.config.from_mapping(
+#         SECRET_KEY='dev',
+#         DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+#     )
+#
+#     if test_config is None:
+#         # load the instance config, if it exists, when not testing
+#         app.config.from_pyfile('config.py', silent=True)
+#     else:
+#         # load the test config if passed in
+#         app.config.from_mapping(test_config)
+#
+#     # ensure the instance folder exists
+#     try:
+#         os.makedirs(app.instance_path)
+#     except OSError:
+#         pass
+#
+#     # a simple page that says hello
+#     @app.route("/")
+#     @app.route("/home")
+#     def home():
+#         return render_template('index.html', posts=posts, title='XiangQi / Chinese Chess')
+#
+#
+#     db.init_app(app)
+#     from . import auth
+#     app.register_blueprint(auth.bp)
+#     return app
--- config.py
+++ config.py
@@ -1,1 +1,17 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+
+    # a cryptographic key, useful to generate signatures or tokens
+    # to protect web forms against a nasty attack called Cross-Site Request Forgery or CSRF
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # Flask-SQLAlchemy extension takes the location of the application's database from
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    # disable a feature of Flask-SQLAlchemy that
+    # signal the application every time a change is about to be made in the database.
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
