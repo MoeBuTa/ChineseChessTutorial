9369a6141fc38e522bcaa87a1509afaadca01535
Wenxiao <64770502+MoeBuTa@users.noreply.github.com>
2021-04-25 17:27:51 +0800

    deleted comments & implemented login&register method

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,14 @@
-# import os
-#
-# import posts as posts
-#
-# from . import db
-# from flask import Flask, render_template
-
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
-# __name__ : predefined variable in python, which is set to the name of the module in which it is used
-#  flask can use it as a starting point of loading associated resources such as template files
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
+login = LoginManager(app)
 
 from app import routes, models
 
@@ -24,36 +16,3 @@ from app import routes, models
 if __name__ == '__main__':
     app.run(debug=True)
 
-
-# def create_app(test_config=None):
-#     # create and configure the app
-#     app = Flask(__name__, instance_relative_config=True)
-#     app.config.from_mapping(
-#         SECRET_KEY='dev',
-#         DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
-#     )
-#
-#     if test_config is None:
-#         # load the instance config, if it exists, when not testing
-#         app.config.from_pyfile('config.py', silent=True)
-#     else:
-#         # load the test config if passed in
-#         app.config.from_mapping(test_config)
-#
-#     # ensure the instance folder exists
-#     try:
-#         os.makedirs(app.instance_path)
-#     except OSError:
-#         pass
-#
-#     # a simple page that says hello
-#     @app.route("/")
-#     @app.route("/home")
-#     def home():
-#         return render_template('index.html', posts=posts, title='XiangQi / Chinese Chess')
-#
-#
-#     db.init_app(app)
-#     from . import auth
-#     app.register_blueprint(auth.bp)
-#     return app
--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from app.models import User
 
 
 # For each field, an object is created as a class variable
@@ -9,5 +10,23 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
-    submit = SubmitField('Sign In')
+    loginSubmit = SubmitField('Sign In')
 
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    registrationSubmit = SubmitField('create account')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
