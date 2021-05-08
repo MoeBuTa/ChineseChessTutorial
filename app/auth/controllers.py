4bb1fa834fa391f5d919896186a7e49f1c2e03a8
Wenxiao <64770502+MoeBuTa@users.noreply.github.com>
2021-05-08 10:26:20 +0800

    fix login/register bugs

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -11,21 +11,22 @@ class UserController:
     @staticmethod
     def login_and_register():
 
-        # if current_user.is_authenticated:
-        #     return redirect(url_for('main.index'))
+        if current_user.is_authenticated:
+            return redirect(url_for('main.index'))
 
         # create login form
         loginForm = LoginForm()
+        # create register form
+        registrationForm = RegistrationForm()
+
         if loginForm.login_submit.data and loginForm.validate():
             user = User.query.filter_by(username=loginForm.login_username.data).first()
             if user is None or not user.check_password(loginForm.login_password.data):
                 flash('Invalid username or password')
-                return redirect(url_for('auth.login_and_register'))
-            login_user(user, remember=loginForm.remember_me.data)
-            return redirect(url_for('main.index'))
+            else:
+                login_user(user, remember=loginForm.remember_me.data)
+                return redirect(url_for('main.index'))
 
-        # create register form
-        registrationForm = RegistrationForm()
         if registrationForm.registration_submit.data:
             flash("Congratulations, you are now a registered user!")
         return render_template('loginAndRegister.html', title='Sign In/up',
