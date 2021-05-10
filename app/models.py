e4ce7cb1d147a3c8f2aa8c0eec3e51863f54da9b
Wenxiao <64770502+MoeBuTa@users.noreply.github.com>
2021-05-10 18:54:34 +0800

    fix area chart bug

--- app/models.py
+++ app/models.py
@@ -164,7 +164,7 @@ class TutorialProgress(db.Model):
             # update time_duration and read_time
             time_duration = (datetime.now() - last_tutorial_progress.last_tutorial_read_time).seconds / 60
             if time_duration < 5:
-                last_tutorial_progress.time_duration = time_duration
+                last_tutorial_progress.time_duration += time_duration
             else:
                 last_tutorial_progress.time_duration = 5
             last_tutorial_progress.last_tutorial_read_time = datetime.now()
