2029ba3e50b688a902fcdd03307f155120b40b08
Wenxiao <64770502+MoeBuTa@users.noreply.github.com>
2021-05-11 18:22:13 +0800

    added system test for register and login (see updated readmd)

--- config.py
+++ config.py
@@ -5,10 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-
     # a cryptographic key, useful to generate signatures or tokens
     # to protect web forms against a nasty attack called Cross-Site Request Forgery or CSRF
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'very-very-secret'
 
     # Flask-SQLAlchemy extension takes the location of the application's database from
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
@@ -21,7 +20,8 @@ class Config(object):
 
 class TestingConfig(Config):
     TESTING = True
+    LIVESERVER_PORT = 11451
+    LIVESERVER_TIMEOUT = 10
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'tests/test.db')
-
     # in memory database
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
